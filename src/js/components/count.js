4aab76b97a2f6580be065edd010e02e8913c3d4a
Andy Bell <me@andy-bell.co.uk>
2018-06-27 20:09:08 +0100

    Improve count's display logic

--- src/js/components/count.js
+++ src/js/components/count.js
@@ -16,11 +16,12 @@ export default class Count extends Component {
      */
     render() {
         let suffix = store.state.items.length !== 1 ? 's' : '';
-        
+        let emoji = store.state.items.length > 0 ? '🙌' : '😢';
+
         this.element.innerHTML = `
             <small>You've done</small>
             <span>${store.state.items.length}</span>
-            <small>thing${suffix} today 🙌</small>
+            <small>thing${suffix} today ${emoji}</small>
         `;
     }
 }
